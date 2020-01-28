d5b5e31c5c3099b27aa97439c086a5e1461314ab
Angela Ayyash <ayyashangela@gmail.com>
2020-01-28 15:39:19 -0500

    loop updates

--- script.js
+++ script.js
@@ -22,15 +22,16 @@ for (let i = 0; i < lifeEvents.length; i++) {
 }
 
 let counter = 0;
-let randomNumber = Math.floor(Math.random() * 10 + 1);
 
 while (true) {
+  let randomNumber = Math.floor(Math.random() * 10 + 1);
   if (randomNumber !== 5) {
+    counter++;
     console.log("randomNumber !== 5");
-    break;
   } else {
+    counter++;
     console.log(
-      "5 === 5. It took counter iterations to randomly generate the number 5."
+      `5 === 5. It took ${counter} iterations to randomly generate the number 5.`
     );
     break;
   }
